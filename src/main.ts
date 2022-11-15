52da29586218a526cf72ae9bf2f56bb1cc9e2502
Printyf <felix.pinty@gmail.com>
2022-11-15 10:29:37 +0100

    refacto

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 export function bowlingScore(results: Array<number>): number {
-    if (results[0] === 10)
-        return 14;
+    let bonus = 1;
+    for (let i = 0; i < results.length; i++) {
+        results[i] *= bonus;
+        if (results[i] === 10) {
+            bonus++;
+        }
+
+    }
     return results.reduce((accumulator, value) => accumulator + value);
 }
